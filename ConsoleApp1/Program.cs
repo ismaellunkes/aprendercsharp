7b49d907a7fc08b7a2b8b91c0da9e859188a30cb
Ismael Lunkes Pereira <ismael.lunkes@outlook.com>
2019-10-20 16:36:41 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Digite um número: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Raiz Quadrada >>> "+Math.Sqrt(x));
+            Console.WriteLine("Potenciação   >>> "+Math.Pow(x, 2));
+
+            Console.Read();
+
+        }
+    }
+}
