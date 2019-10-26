67ce3d32452f6f7babd02e0730d00270a1819082
Ismael Lunkes Pereira <ismael.lunkes@outlook.com>
2019-10-26 00:26:59 -0300

    Versão com matrizes;

---
+++ ConsoleApp1/Matriz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Matriz
+{
+    double[,] mat;
+    public Matriz()
+    {        
+    }
+    public Matriz(int lin, int col)
+    {
+        mat = new double[lin, col];
+
+    }
+
+    public void AddValor(int lin, int col, double valor)
+    {
+        if (mat != null)
+        {
+            mat[lin, col] = valor;
+        }
+    }
+
+    public void RemoveValor(int lin, int col, double valor)
+    {
+        if (mat != null)
+        {
+            mat[lin, col] = 0f;
+        }
+    }
+    
+    public double MontarMatriz(int lin, int col)
+    {
+        if (mat != null)
+        {
+            return mat[lin, col];
+        }
+        else
+        {
+            Console.WriteLine("MATRIZ NÃO DEFINIDA!!!");
+            return 0;
+        }
+
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,169 +5,231 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
 
-            string numeroCC;
-            string nomeTitular;
-            double saldoInicial;
-            string option;
+            /* INÍCIO DO ALGORITMO DA CLASSE MATRIZES  */
 
-            Console.WriteLine("Informe os dados solicitados: ");
-            Console.Write("Informe o número da conta >>>>  ");
-            numeroCC = Console.ReadLine();
-            Console.Write("Informe o titular da conta >>>>  ");
-            nomeTitular = Console.ReadLine();
+            int linhas, colunas;
 
-            Console.Write("Informe se terá depósito incial >>> (S) ou (N)  ");
-            if (Console.ReadLine() == "S")
+            Console.WriteLine("Digite as dimensões da MATRIZ: (Linhas X Colunas) ");            
+            Console.Write("LINHAS  >>>>> ");
+            linhas = int.Parse(Console.ReadLine());
+            Console.Write("COLUNAS >>>>> ");
+            colunas = int.Parse(Console.ReadLine());
+
+            Matriz mat = new Matriz(linhas, colunas);
+
+            for (int x = 0; x < linhas; x++)
             {
-                Console.Write("Informe o valor >>> ");
-                saldoInicial = double.Parse(Console.ReadLine());
+                for (int y = 0; y < colunas; y++)
+                {
+                    Console.Write("["+x+"]["+y+"] >>> ");
+                    mat.AddValor(x, y, double.Parse(Console.ReadLine()));                   
+
+                }
             }
-            else
+            Console.WriteLine("   ");
+            Console.WriteLine("\n----- EXIBINDO A MATRIZ INFORMADA ---------   ");
+            Console.WriteLine("   ");
+            
+            for (int x = 0; x < linhas; x++)
             {
-                saldoInicial = 0;
-            }
-
-            Conta conta = new Conta(numeroCC, nomeTitular, saldoInicial);            
-
-            Console.WriteLine("Informe a operação: ");
-            Console.WriteLine("1 - Saque ");
-            Console.WriteLine("2 - Depósito ");
-            Console.WriteLine("3 - Consulta de saldo ");
-            Console.WriteLine("4 - Extrato ");
-            Console.WriteLine("5 - Resumo da conta ");
-            Console.WriteLine("6 - Sair ");
-
-            option = Console.ReadLine();
-
-            while (option != "6")
-            {
-                if (option == "1")
+                for (int y = 0; y < colunas; y++)
                 {
-                    Console.WriteLine("Informe o valor do saque >>>> ");
-                    conta.AtualizarSaldo(double.Parse(Console.ReadLine()) * -1);
-                }
-                if (option == "2")
-                {
-                    Console.WriteLine("Informe o valor do depósito >>>> ");
-                    conta.AtualizarSaldo(double.Parse(Console.ReadLine()));
-                }
-                if (option == "3")
-                {
-                    Console.WriteLine("Saldo Atualizado >>>> " + conta.SaldoAtualizado);
-                }
-                if (option == "4")
-                {                    
-                    conta.GerarExtrato();                    
-                }
-                
-                if (option == "5")
-                {
-                    Console.WriteLine(conta.toString());
-                }
-                Console.WriteLine("\n\n\n Informe a operação: ");
-                Console.WriteLine("1 - Saque ");
-                Console.WriteLine("2 - Depósito ");
-                Console.WriteLine("3 - Consulta de saldo ");
-                Console.WriteLine("4 - Extrato ");
-                Console.WriteLine("5 - Resumo da conta ");
-                Console.WriteLine("6 - Sair ");
-
-                option = Console.ReadLine();
-            }                                    
-
-        }
-
-         /*  ForkGame game1 = new ForkGame();
-            int f = 1;
-            String palavraSorteada;
-            while (f == 1)
-            {
-                Console.WriteLine("");
-                palavraSorteada = game1.SortearPalavra();
-                Console.WriteLine(game1.MostrarCaracteres(palavraSorteada));
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                int x = 5;
-                char letra;
-
-                Console.WriteLine("Você tem " + x + " chances!");
-                Console.WriteLine("  ");
-                while (x > 0)
-                {
-                    Console.Write("DÊ seu palpite...>>>  ");
-                    letra = char.Parse(Console.ReadLine());
-                    if (game1.ExisteLetra(palavraSorteada, letra))
-                    {
-                        Console.WriteLine("CORRETO!!!");
-                        Console.WriteLine("  ");
-                        Console.WriteLine("  ");
-                    }
-                    else
-                    {
-                        x--;
-                        Console.WriteLine("ERRROOOUU!!  ");
-                        Console.WriteLine("  ");
-                        Console.WriteLine("  ");
-                        Console.WriteLine("Você tem " + x + " chance(s)!");
-                        Console.WriteLine("  ");                     
-                        
-                    }   
                     
-                }
+                    Console.Write("  "+mat.MontarMatriz(x,y));
+                                       
+                    if (mat.MontarMatriz(x, y) > -9 && mat.MontarMatriz(x, y) <= 9)
+                    {
+                        Console.Write("            ");
+                    }
+                    if (mat.MontarMatriz(x, y) > 9 && mat.MontarMatriz(x, y) <= 99)
+                    {
+                        Console.Write("           ");
+                    }
+                    if (mat.MontarMatriz(x, y) > 99 && mat.MontarMatriz(x, y) <= 999)
+                    {
+                        Console.Write("          ");
+                    }
+                    if (mat.MontarMatriz(x, y) > 999 && mat.MontarMatriz(x, y) <= 9999)
+                    {
+                        Console.Write("         ");
+                    }
 
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                Console.WriteLine("***** GAME OVER *******");
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                Console.WriteLine("  ");
-                Console.WriteLine("Digite 1 para continuar!");
-                f = int.Parse(Console.ReadLine());
-            }           
+                    if (y == colunas - 1)
+                    {
+                        Console.Write("\n");
+                    }
+
+
+                }
+            }
+
+            Console.ReadLine();
+
+
+
+            /* string numeroCC;
+             string nomeTitular;
+             double saldoInicial;
+             string option;
+
+             Console.WriteLine("Informe os dados solicitados: ");
+             Console.Write("Informe o número da conta >>>>  ");
+             numeroCC = Console.ReadLine();
+             Console.Write("Informe o titular da conta >>>>  ");
+             nomeTitular = Console.ReadLine();
+
+             Console.Write("Informe se terá depósito incial >>> (S) ou (N)  ");
+             if (Console.ReadLine() == "S")
+             {
+                 Console.Write("Informe o valor >>> ");
+                 saldoInicial = double.Parse(Console.ReadLine());
+             }
+             else
+             {
+                 saldoInicial = 0;
+             }
+
+             Conta conta = new Conta(numeroCC, nomeTitular, saldoInicial);            
+
+             Console.WriteLine("Informe a operação: ");
+             Console.WriteLine("1 - Saque ");
+             Console.WriteLine("2 - Depósito ");
+             Console.WriteLine("3 - Consulta de saldo ");
+             Console.WriteLine("4 - Extrato ");
+             Console.WriteLine("5 - Resumo da conta ");
+             Console.WriteLine("6 - Sair ");
+
+             option = Console.ReadLine();
+
+             while (option != "6")
+             {
+                 if (option == "1")
+                 {
+                     Console.WriteLine("Informe o valor do saque >>>> ");
+                     conta.AtualizarSaldo(double.Parse(Console.ReadLine()) * -1);
+                 }
+                 if (option == "2")
+                 {
+                     Console.WriteLine("Informe o valor do depósito >>>> ");
+                     conta.AtualizarSaldo(double.Parse(Console.ReadLine()));
+                 }
+                 if (option == "3")
+                 {
+                     Console.WriteLine("Saldo Atualizado >>>> " + conta.SaldoAtualizado);
+                 }
+                 if (option == "4")
+                 {                    
+                     conta.GerarExtrato();                    
+                 }
+
+                 if (option == "5")
+                 {
+                     Console.WriteLine(conta.toString());
+                 }
+                 Console.WriteLine("\n\n\n Informe a operação: ");
+                 Console.WriteLine("1 - Saque ");
+                 Console.WriteLine("2 - Depósito ");
+                 Console.WriteLine("3 - Consulta de saldo ");
+                 Console.WriteLine("4 - Extrato ");
+                 Console.WriteLine("5 - Resumo da conta ");
+                 Console.WriteLine("6 - Sair ");
+
+                 option = Console.ReadLine();
+             }     */
+
+        }
+
+        /*  ForkGame game1 = new ForkGame();
+           int f = 1;
+           String palavraSorteada;
+           while (f == 1)
+           {
+               Console.WriteLine("");
+               palavraSorteada = game1.SortearPalavra();
+               Console.WriteLine(game1.MostrarCaracteres(palavraSorteada));
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               int x = 5;
+               char letra;
+
+               Console.WriteLine("Você tem " + x + " chances!");
+               Console.WriteLine("  ");
+               while (x > 0)
+               {
+                   Console.Write("DÊ seu palpite...>>>  ");
+                   letra = char.Parse(Console.ReadLine());
+                   if (game1.ExisteLetra(palavraSorteada, letra))
+                   {
+                       Console.WriteLine("CORRETO!!!");
+                       Console.WriteLine("  ");
+                       Console.WriteLine("  ");
+                   }
+                   else
+                   {
+                       x--;
+                       Console.WriteLine("ERRROOOUU!!  ");
+                       Console.WriteLine("  ");
+                       Console.WriteLine("  ");
+                       Console.WriteLine("Você tem " + x + " chance(s)!");
+                       Console.WriteLine("  ");                     
+
+                   }   
+
+               }
+
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               Console.WriteLine("***** GAME OVER *******");
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               Console.WriteLine("  ");
+               Console.WriteLine("Digite 1 para continuar!");
+               f = int.Parse(Console.ReadLine());
+           }           
+       }
+
+
+
+       /** char op, power = '1';
+        double x, y;
+
+        Console.WriteLine("CALCULATOR OF THE CHUCK NORRIS");
+
+        while (power.Equals('1'))
+        {
+            x = double.Parse(Console.ReadLine());
+            op = char.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            Calculadora.Calcular(x, op, y);
+            Console.WriteLine("---");
+            Console.WriteLine(Calculadora.resultado);
+            Console.WriteLine("DIGITE 1 CONTINUAR");
+            power = char.Parse(Console.ReadLine());
+
         }
 
 
+        /**
+            int qtde;
 
-        /** char op, power = '1';
-         double x, y;
+            Produto produto = new Produto();
+            produto.nome = Console.ReadLine();
+            produto.preco = double.Parse(Console.ReadLine());
+            produto.quantidade = int.Parse(Console.ReadLine());
 
-         Console.WriteLine("CALCULATOR OF THE CHUCK NORRIS");
+            Console.WriteLine(produto);
 
-         while (power.Equals('1'))
-         {
-             x = double.Parse(Console.ReadLine());
-             op = char.Parse(Console.ReadLine());
-             y = double.Parse(Console.ReadLine());
-             Calculadora.Calcular(x, op, y);
-             Console.WriteLine("---");
-             Console.WriteLine(Calculadora.resultado);
-             Console.WriteLine("DIGITE 1 CONTINUAR");
-             power = char.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade a ser adicionada: ");
+            qtde = int.Parse(Console.ReadLine());
+            produto.AdicionarProduto(qtde);
 
-         }
-
-
-         /**
-             int qtde;
-
-             Produto produto = new Produto();
-             produto.nome = Console.ReadLine();
-             produto.preco = double.Parse(Console.ReadLine());
-             produto.quantidade = int.Parse(Console.ReadLine());
-
-             Console.WriteLine(produto);
-
-             Console.Write("Digite a quantidade a ser adicionada: ");
-             qtde = int.Parse(Console.ReadLine());
-             produto.AdicionarProduto(qtde);
-
-             Console.WriteLine("Produto atualizado ");
-             Console.Write(produto);
-         */
+            Console.WriteLine("Produto atualizado ");
+            Console.Write(produto);
+        */
 
     }
 }
